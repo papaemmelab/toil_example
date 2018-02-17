9d7d97664e09c43e597facacd266a07f4371ff15
Travis CI <travis@travis-ci.org>
2018-02-17 16:28:21 -0500

    start from scracth

--- tests/test_version.py
+++
@@ -1,8 +0,0 @@
-"""toil_example version test."""
-
-from toil_example import __version__
-
-
-def test_version():
-    """Sample test for the __version__ variable."""
-    assert __version__ == "0.1.0"
