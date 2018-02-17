5e8be46a2e4bb97c2b73aff1985d367e612f686d
Travis CI <travis@travis-ci.org>
2018-02-17 16:29:51 -0500

    first commit

---
+++ tests/test_commands.py
@@ -0,0 +1,34 @@
+"""=toil_example commands tests."""
+
+from os.path import join
+
+import pytest
+
+from toil_example import commands
+
+
+def test_run_toil(tmpdir):
+    """Sample test for the main command."""
+    #  Define arguments.
+    message = "This is a test message for the Universe."
+    logfile = tmpdir.join("log.txt")
+    jobstore = tmpdir.join("jobstore")
+    total = 3
+    args = [
+        jobstore.strpath,
+        "--message", message,
+        "--total", str(total),
+        "--logFile", logfile.strpath,
+        ]
+
+    # Get and validate options.
+    parser = commands.get_parser()
+    options = parser.parse_args(args)
+    options = commands.process_parsed_options(options)
+
+    # Call pipeline
+    commands.run_toil(options)
+
+    # Assert custom message is echoed in master log.
+    with open(logfile.strpath) as f:
+        assert len(f.read().split(message)) == total + 1
